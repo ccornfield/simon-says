7a235d9ba0a5251d1f1c56de8c3523ca861fece7
ecolus <ecolus48@gmail.com>
2024-05-31 12:18:26 +0100

    Finished Project

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,8 @@ let game = {
     playerMoves: [],
     score: 0,
     turnNumber: 0,
+    lastButton: "",
+    turnInProgress: false,
     choices: ["button1", "button2", "button3", "button4"]
 };
 
@@ -10,15 +12,19 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     game.score = 0;
+
     for (let circle of document.getElementsByClassName("circle")) {
-        if (circle.getAttribute("data-listener") != "true") {
+        if (circle.getAttribute("data-listener") !== "true") {
             circle.addEventListener("click", (e) => {
-                let move = e.target.getAttribute("id");
-                lightsOn(move);
-                game.playerMoves.push(move);
-                playerTurn();
+                if (game.currentGame.length > 0 && !game.turnInProgress) {
+                    let move = e.target.getAttribute("id");
+                    game.lastButton = move;
+                    game.playerMoves.push(move);
+                    lightsOn(move);
+                    playerTurn();
+                }
             });
-            circle.setAttribute("data-listener", "true")
+            circle.setAttribute("data-listener", "true");
         }
     }
     showScore();
@@ -32,12 +38,14 @@ function addTurn() {
 }
 
 function showTurns() {
+    game.turnInProgress = true;
     game.turnNumber = 0;
     let turns = setInterval(function () {
         lightsOn(game.currentGame[game.turnNumber]);
         game.turnNumber++;
         if (game.turnNumber >= game.currentGame.length) {
             clearInterval(turns);
+            game.turnInProgress = false;
         }
     }, 800);
 }
@@ -49,14 +57,10 @@ function lightsOn(circ) {
     }, 400);
 }
 
-function showScore() {
-    document.getElementById("score").innerText = game.score;
-}
-
 function playerTurn() {
     let i = game.playerMoves.length - 1;
     if (game.currentGame[i] === game.playerMoves[i]) {
-        if (game.currentGame.length === game.playerMoves.length){
+        if (game.currentGame.length === game.playerMoves.length) {
             game.score++;
             showScore();
             addTurn();
@@ -65,6 +69,10 @@ function playerTurn() {
         alert("Wrong move!");
         newGame();
     }
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
 }
 
 module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
